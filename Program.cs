a147a48f21fa0443674bf2f6aea73ddfa08e71cb
Evgenia <kolontai.e.v@yandex.ru>
2022-01-25 12:20:50 +0300

    Создан код

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// Console.Clear();
+//  string[] ArrayB = {"Hello", "2", "world", ":-)"};
+// Console.WriteLine(Words);
+// Console.Write("[");
+// for (int i=0; i < Words.Length; i++)
+// {
+// //     if (Words[i].Length <= 3)
+//     Console.Write(" " + Words[i]+ " ");
+// }
+// Console.WriteLine("]");
+
+// string[] Dev2(string[] Words)
+// {
+// string[] ArrayA = new string[0];
+// int j= 0;
+//     for (int i = 0; i < Words.Length; i++)
+    
+//         if ( Words[i].Length <= 3)
+//         {
+//             int lenght =ArrayA.Length;
+//             Array.Resize(ref ArrayA, lenght +1);
+//             Words[i] = ArrayA[j];
+//             j++;
+
+//         }
+//     return(ArrayA); 
+// }
+// void WriteArray(string[] Words, string[] ArrayA)
+
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < Words.Length; i++)
+//     {
+//         Console.Write(Words[i]);
+//         if (i != Words.Length - 1)
+//         {
+//             Console.Write(", ");
+//         }
+//     }
+//     Console.Write("] -> [");
+//     for (int i = 0; i < ArrayA.Length; i++)
+//     {
+//         Console.Write(ArrayA[i]);
+//         if (i != ArrayA.Length - 1)
+//         {
+//             Console.Write(", ");
+//         }
+//     }
+//     Console.WriteLine("]");
+// }
+
+// WriteArray(Words, Dev2(Words));
+
+
+Console.Clear();
+string[] ArrayA = {"Hello", "2", "world", ":-)","ghgh","hg"};
+string[] Find(string[] ArrayA)
+{
+    string[] ArrayB = new string[0];
+    int j = 0;
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        if (ArrayA[i].Length <= 3)
+        {
+            int lenght = ArrayB.Length;
+            Array.Resize(ref ArrayB, lenght + 1);
+            ArrayB[j] = ArrayA[i];
+            j++;
+
+        }
+
+    }
+    return (ArrayB); 
+}
+void WriteArray(string[] ArrayA, string[] ArrayB)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        Console.Write(ArrayA[i]);
+        if (i != ArrayA.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("] -> [");
+    for (int i = 0; i < ArrayB.Length; i++)
+    {
+        Console.Write(ArrayB[i]);
+        if (i != ArrayB.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+WriteArray(ArrayA, Find(ArrayA));
